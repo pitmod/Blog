c2997f126254dd688bd35c4c62064269152f3219
Piotr Modlinger <piotr.modlinger@gmail.com>
2017-12-08 21:52:49 +0100

    Functional Poll app. settings.py modified

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,16 +73,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Mysite',
-	'USER': 'mysiteuser',
-	'PASSWORD': 'password',
-	'HOST': 'localhost',
-	'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+##DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'Mysite',
+#	'USER': 'mysiteuser',
+#	'PASSWORD': 'password',
+#	'HOST': 'localhost',
+#	'PORT': '',
+#    }
+#}
 
 
 # Password validation
